987644780d11d9f61dd83142b77dadb6da392b1e
nauco <lhdong@mz.co.kr>
2022-11-21 13:33:11 +0900

    credential count 추가

--- codebuild.tf
+++ codebuild.tf
@@ -160,6 +160,8 @@ resource "aws_codebuild_project" "codebuild" {
 }
 
 resource "aws_codebuild_source_credential" "bitbucket" {
+  count       = var.aws_codebuild_source_credential_bitbucket_token == "" ? 0 : 1
+  
   auth_type   = "BASIC_AUTH"
   server_type = "BITBUCKET"
   token       = var.aws_codebuild_source_credential_bitbucket_token
